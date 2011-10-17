0db3908b5c91a8a72f34bfa5a7391ca134add1cf
Tejus Kudari <tkudari@gmail.com>
2011-10-17 00:35:27 -0700

    More cleanup

--- src/com/tejus/shavedog/activity/ShaveDogActivity.java
+++ src/com/tejus/shavedog/activity/ShaveDogActivity.java
@@ -1,56 +1,29 @@
 package com.tejus.shavedog.activity;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-
 import com.tejus.shavedog.Definitions;
 import com.tejus.shavedog.R;
 
 import com.tejus.shavedog.ShaveService;
 
-import com.tejus.shavedog.ShaveService.ShaveBinder;
-
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.DatabaseUtils;
-import android.media.ExifInterface;
+
 import android.net.DhcpInfo;
-import android.net.ParseException;
-import android.net.Uri;
+
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.os.Environment;
+
 import android.os.IBinder;
-import android.provider.MediaStore.Images;
-import android.provider.MediaStore.MediaColumns;
-import android.provider.MediaStore.Video;
-import android.telephony.gsm.SmsManager;
-import android.text.format.Time;
-import android.util.Base64;
+
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -59,26 +32,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class ShaveDogActivity extends Activity {
-    private static final String[] PROJECTION;
-    static String algorithm = "SHA1";
 
-    private StringBuilder sBuilder;
     private Context mContext;
     private BroadcastReceiver mShaveReceiver = new ServiceIntentReceiver();
 
-    static int _16MB_ = 16 * 1000000;
-    static {
-        PROJECTION = new String[] {
-            MediaColumns._ID,
-            MediaColumns.DATA,
-            MediaColumns.DATE_ADDED,
-            MediaColumns.DATE_MODIFIED,
-            Images.ImageColumns.DATE_TAKEN,
-            MediaColumns.DISPLAY_NAME,
-            MediaColumns.MIME_TYPE,
-            MediaColumns.SIZE
-        };
-    }
     TextView details, welcome;
     WifiManager wifi;
     DhcpInfo dhcp;
@@ -88,19 +45,16 @@ public class ShaveDogActivity extends Activity {
     @Override
     public void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
-        Log.d( "XXXX", "oncreate called too" );
         setContentView( R.layout.main );
         mContext = this;
         details = ( TextView ) findViewById( R.id.details );
         welcome = ( TextView ) findViewById( R.id.welcome );
-        sBuilder = new StringBuilder();
         initShaveServiceStuff();
         initReceiver();
     }
 
     @Override
     protected void onResume() {
-        Log.d( "XXXX", "onresume called.." );
         super.onResume();
     }
 
@@ -132,33 +86,14 @@ public class ShaveDogActivity extends Activity {
             case R.id.set_creds:
                 this.setCredentials();
                 return true;
-                
+
             case R.id.friends_list:
                 this.gotoFriendsList();
                 return true;
 
-              
-
             default:
                 return super.onOptionsItemSelected( item );
         }
-    }
-
-    
-
-    @SuppressWarnings( "deprecation" )
-    void send( String address, String body ) {
-        SmsManager smsManager = SmsManager.getDefault();
-
-        PendingIntent sentPI = PendingIntent.getBroadcast( this, 0, new Intent( "SENT" ), 0 );
-        ArrayList<String> messageArray = smsManager.divideMessage( body );
-        ArrayList<PendingIntent> sentIntents = new ArrayList<PendingIntent>();
-        for ( int i = 0; i < messageArray.size(); i++ ) {
-            sentIntents.add( sentPI );
-        }
-        Log.d( "XXXX", "gonna sendMultipartTextMessage..." );
-        smsManager.sendMultipartTextMessage( address, null, messageArray, sentIntents, null );
-
     }
 
     private void gotoFriendsList() {
@@ -183,8 +118,6 @@ public class ShaveDogActivity extends Activity {
         return InetAddress.getByAddress( quads );
     }
 
-    
-
     void quit() {
         Log.d( "XXXX", "quit(): Killing ourself.." );
         android.os.Process.killProcess( android.os.Process.myPid() );
@@ -195,22 +128,6 @@ public class ShaveDogActivity extends Activity {
         intent.setClass( mContext, CredentialsActivity.class );
         startActivity( intent );
     }
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
 
     void initShaveServiceStuff() {
         mConnection = new ServiceConnection() {
@@ -223,13 +140,10 @@ public class ShaveDogActivity extends Activity {
             @Override
             public void onServiceConnected( ComponentName name, IBinder service ) {
                 mShaveService = ( ( ShaveService.ShaveBinder ) service ).getService();
-                // Toast.makeText( mContext, R.string.shave_service_connected,
-                // Toast.LENGTH_SHORT ).show();
             }
         };
 
         doBindService();
-
         startService( new Intent().setClass( mContext, ShaveService.class ) );
     }
 
@@ -286,50 +200,4 @@ public class ShaveDogActivity extends Activity {
         unregisterReceiver( mShaveReceiver );
     }
 
-    void testApi2() {
-        Uri sms = Uri.parse( "content://sms/" );
-        Cursor c = this.getContentResolver().query( sms, null, null, null, null );
-        Log.d( "XXXX", "gonna start dumping the cursor" );
-        DatabaseUtils.dumpCursor( c );
-    }
 }
-
-// test code:
-// try {
-// ExifInterface exif = new ExifInterface(
-// mediaCursor.getString( 1 ) );
-// String exifDateTime = exif.getAttribute(
-// ExifInterface.TAG_DATETIME );
-// Log.d( "XXXX", "exifDateTime = " + exifDateTime );
-// DateFormat format = new
-// SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-// try {
-// Date date = format.parse( exifDateTime );
-// } catch (java.text.ParseException e) {
-// e.printStackTrace();
-// }
-// /* if ( exifDateTime != null ) {
-// Log.d( "XXXX", "parsed date = " + Date.parse(
-// exifDateTime ) );
-// } else {
-// Log.d( "XXXX", "exifDateTime is null" );
-// } */
-// } catch ( IOException e ) {
-// Log.d( "XXXX",
-// "MediaAccessor2.readAtCursor: Error trying to create ExifInterface.."
-// );
-// e.printStackTrace();
-// }
-
-/*
- * try { ExifInterface exif = new ExifInterface( filepath ); String exifDateTime
- * = exif.getAttribute( ExifInterface.TAG_DATETIME ); logger.info(
- * "XXXX - exifDateTime = " + exifDateTime ); } catch ( IOException e ) {
- * logger.error(
- * "MediaAccessor2.readAtCursor: Error trying to create ExifInterface.." );
- * e.printStackTrace(); }
- */
-
-// return Base64.encodeToString(md.digest(), Base64.NO_PADDING | Base64.NO_WRAP
-// | Base64.URL_SAFE);
-
